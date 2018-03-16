02882e4e44bd082511749644c041e88afc239cc3
Keegan Obonyo <keeganobonyo@gmail.com>
2018-03-16 22:25:19 +0300

    u rls refactored to reflect changes

--- mpesa/urls.py
+++ mpesa/urls.py
@@ -1,18 +1,16 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import url
-from .views import authenticate, transactions_record
+from .views import (authenticate, create_transaction)
 
 urlpatterns = [
     url(r'^get_token/$',
         view=authenticate,
         name="get-mpesa-token-request",),
-    url(r'^get_token/$',
-        view=transactions_record,
-        name="add-transaction-response",),
+    # url(r'^create_response/$',
+    #     view=transactions_record,
+    #     name="add-transaction-response",),
+    url(r'^create_transaction/$',
+        view=create_transaction,
+        name="create_transaction",),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json'])
-
-"""    url(r'^request/delete/(?P<permission_pk>\d+)/$',
-        view=delete_permission,
-        name="authority-delete-permission-request",
-        kwargs={'approved': False}),"""
