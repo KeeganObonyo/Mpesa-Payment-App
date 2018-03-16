0399662db235b1c47e0e5c45adbbf1444c2869be
Keegan Obonyo <keeganobonyo@gmail.com>
2018-03-16 22:24:10 +0300

    mpesa app db refactored to include multiple transactions

--- mpesa/models.py
+++ mpesa/models.py
@@ -5,34 +5,55 @@ from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 
-"""class MpesaCommandId(model.Models):
-    CHOICES = Choices(
-        (1, 'TransactionReversal'),  # Reversal
-        (2, 'SalaryPayment'),  # employer to employees
-        (3, 'BusinessPayment'),  # business to customer
-        (4, 'PromotionPayment'),  # money on promotions
-        (5, 'AccountBalance'),
-        (6, 'CustomerPayBillOnline'),
-        # Used to simulate a transaction taking place in the case of C2B
-        # Simulate Transaction or to initiate a transaction on behalf of the
-        # customer (STK Push).
-        (7, 'TransactionStatusQuery'),  # query the details of a transaction.
-        (8, 'CheckIdentity'),  # Smlr to STK, M-Pesa PIN as service.
-        (9, 'BusinessPayBill'),  # one paybill to another paybill
-        (10, 'BusinessBuyGoods'),  # buy goods to another buy goods
-        (11, 'DisburseFundsToBusiness'),  # funds from utility to MMF account.
-        (12, 'BusinessToBusinessTransfer'),  # MMF 1 2 o paybill MMF account.
-        (13, 'BusinessTransferFromMMFToUtilitys'))  # Transferring funds from paybills MMF to another paybills utility account.
-    name = models.CharField(max_length=200, choices=CHOICES)
+class MpesaCommandId(models.Model):
+    name = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.name)
 
-"""
+
+class CompanyShortCode(models.Model):
+    name = models.IntegerField(null=True)
+
+    def __str__(self):
+        return str(self.name)
 
 
-class Transactions(models.Model):
-    response = models.CharField(max_length=200, db_index=True, blank=True)
+class InitiatorName(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class TransactionType(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Transaction(models.Model):
+    transaction_type = models.ForeignKey(TransactionType,
+                                         related_name='type', null=True)
+    command_id = models.ForeignKey(MpesaCommandId,
+                                   related_name='command_id', null=True)
+    amount = models.DecimalField(null=True, decimal_places=2, max_digits=6)
+    comments = models.CharField(max_length=200, null=True)
+    phoneno = models.IntegerField(null=True)
+    initiator_name = models.ForeignKey(InitiatorName,
+                                       related_name='company_name', null=True)
+    shortcode = models.ForeignKey(CompanyShortCode,
+                                  related_name='short_code')
+
+    def __str__(self):
+        return str(self.transaction_type)
+
+
+class TransactionResponse(models.Model):
+    transaction_feedbacks = models.CharField(max_length=200, blank=True)
+    transaction = models.ForeignKey(Transaction,
+                                    related_name='response', null=True)
 
     def __str__(self):
         return str(self.response)
