8c6100f5b4dbfe33bf6ae807819800f9ab8b0c30
Keegan Obonyo <keeganobonyo@gmail.com>
2018-03-19 18:20:48 +0300

    required changes following the additions

--- PAYMENT_SYSTEM/urls.py
+++ PAYMENT_SYSTEM/urls.py
@@ -19,5 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^mpesa/', include('mpesa.urls'))
-
 ]
--- mpesa/urls.py
+++ mpesa/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import url
-from .views import (authenticate, create_transaction)
+from .views import *
 
 urlpatterns = [
     url(r'^get_token/$',
@@ -9,8 +9,8 @@ urlpatterns = [
     # url(r'^create_response/$',
     #     view=transactions_record,
     #     name="add-transaction-response",),
-    url(r'^create_transaction/$',
-        view=create_transaction,
+    url(r'^create_b_to_b_transaction/$',
+        view=create_b_to_b_transaction,
         name="create_transaction",),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json'])
