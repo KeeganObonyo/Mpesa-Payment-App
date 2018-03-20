436f7d939ed3770c3c3ac8b73252bad1dfe102ba
Keegan Obonyo <keeganobonyo@gmail.com>
2018-03-20 15:41:25 +0300

    null

--- mpesa/urls.py
+++ mpesa/urls.py
@@ -14,3 +14,41 @@ urlpatterns = [
         name="create_transaction",),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json'])
+
+# url end points to be effected
+
+# create_b_to_c_transaction
+# create_b_to_b_transaction
+# register_c_2_b_url
+# check_account_balance
+# check_transaction_status
+# transaction_reversal
+# initiate_lipa_na_mpesa_online_transaction
+# query_lipa_na_mpesa_online_transaction_status
+
+# create_occasion
+# create_mpesa_command_id
+# create_company_short_code_or_number
+# create_initiator_name
+# create_transaction_type
+# create_customer
+# create_identifier_type
+
+# OccasionDetailAPIView
+# MpesaCommandIdDetailAPIView
+# MpesaShortCodeOrNumberDetailAPIView
+# InitiatorNameDetailAPIView
+# TransactionTypeDetailAPIView
+# CustomerDetailAPIView
+# IdentifierTypeDetailAPIView
+
+# OccasionListView
+# MpesaCommandIdListView
+# MpesaShortCodeOrNumberListView
+# InitiatorNameListView
+# TransactionTypeListView
+# CustomerListView
+# IdentifierTypeListView
+# TransactionListView
+# TransactionResponseListView
+# RegistrationListView
