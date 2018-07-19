d71f9e743307b5be84bcaea9f8e72e4a7853243b
pmutua <pmutua@live.com>
2018-07-19 09:45:56 +0300

    add remarks variable to CreateBToCTransaction API View #4

--- mpesa/views.py
+++ mpesa/views.py
@@ -41,7 +41,7 @@ class CreateBToCTransaction(APIView):
                     id=request.data['phone_no']),
                 transaction = Transaction.objects.create(
                     amount=amount,
-                    comments=comments,
+                    remarks=remarks,
                     party_b=party_b,
                     Party_a=Party_a,
                     command_id=command_id,
