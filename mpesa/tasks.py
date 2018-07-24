aaa829c2c34b9dcc24e0eb3a40ac17eeea06dffe
keegan <keeganobonyo@gmail.com>
2018-07-24 09:33:03 +0300

    asynchronous tasks added for b2c and b2b transaction views

--- mpesa/tasks.py
+++ mpesa/tasks.py
@@ -6,11 +6,39 @@ from rest_framework.response import Response
 from .models import TransactionResponse
 
 @task
-def send_create_b2c_transaction(request):
+def send_create_b2c_transaction(request,access_token):
     """
     Task to send create b2c transaction request
     """
     api_url = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest"
+    headers = {"Authorization": "Bearer %s" % access_token}
+
+    response = requests.post(api_url, json=request, headers=headers)
+    response_description = response['ResponseDescription']
+    originator_conversation_id = response['OriginatorConversationID ']
+    conversation_id = response['ConversationID']
+    merchant_request_id = response['MerchantRequestID']
+    checkout_request_id = response['CheckoutRequestID']
+    response_code = response['ResponseCode']
+    result_description = response['ResultDesc']
+    result_code = response['ResultCode']
+    TransactionResponse.objects.create(
+        transaction_feedback=response_description,
+        transaction=transaction,
+        originator_conversation_id=originator_conversation_id,
+        conversation_id=conversation_id,
+        merchant_request_id=merchant_request_id,
+        checkout_request_id=checkout_request_id,
+        response_code=response_code,
+        result_description=result_description,
+        result_code=result_code)
+
+@task
+def send_create_b2b_transaction(request,access_token):
+    """
+    Task to send create b2b transaction request asynchronously
+    """
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest"
     headers = {"Authorization": "Bearer %s" % access_token}
 
     response = requests.post(api_url, json=request, headers=headers)
--- mpesa/views.py
+++ mpesa/views.py
@@ -76,7 +76,7 @@ class CreateBToCTransaction(APIView):
                 "ResultURL": "/",
                 "Occasion": occ
             }
-            send_create_b2c_transaction(request)
+            send_create_b2c_transaction.delay(request,access_token)
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(responses, status=status.HTTP_201_CREATED)
@@ -124,8 +124,6 @@ class CreateBToBTransaction(APIView):
                 id_type_b = identifier_type_b.name
             except:
                 raise Http404
-            api_url = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest"
-            headers = {"Authorization": "Bearer %s" % access_token}
             request = {
                 "Initiator": name,
                 "SecurityCredential": initiator,
@@ -140,26 +138,7 @@ class CreateBToBTransaction(APIView):
                 "QueueTimeOutURL": "/",
                 "ResultURL": "/"
             }
-
-            response = requests.post(api_url, json=request, headers=headers)
-            response_description = response['ResponseDescription']
-            originator_conversation_id = response['OriginatorConversationID ']
-            conversation_id = response['ConversationID']
-            merchant_request_id = response['MerchantRequestID']
-            checkout_request_id = response['CheckoutRequestID']
-            response_code = response['ResponseCode']
-            result_description = response['ResultDesc']
-            result_code = response['ResultCode']
-            TransactionResponse.objects.create(
-                transaction_feedback=response_description,
-                transaction=transaction,
-                originator_conversation_id=originator_conversation_id,
-                conversation_id=conversation_id,
-                merchant_request_id=merchant_request_id,
-                checkout_request_id=checkout_request_id,
-                response_code=response_code,
-                result_description=result_description,
-                result_code=result_code)
+            send_create_b2b_transaction.delay(request,access_token)
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(responses, status=status.HTTP_201_CREATED)
